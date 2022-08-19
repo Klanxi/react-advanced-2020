462a1f5882c8d62f0accbd2464a55281f590b9a0
Klanxi <xriunia@gmail.com>
2022-08-19 17:05:50 +0300

    tutorial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+/* import FinalSetup from './tutorial/1-useState/final/1-error-example' */
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup/>
+    
     </div>
   )
 }
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text,setText] = useState('Random title')
+  const handleClick = () =>{
+    if(text === 'random title'){
+      setText('Hello world')
+    } else {
+      setText('random title')
+    }
+    
+  }
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className='btn' onClick={handleClick}>Change title</button>
+    </React.Fragment>;
+
 };
 
 export default UseStateBasics;
